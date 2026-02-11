8f78b458628ed037d21a78a3af3a6f2ab42c33f2
PRADEESH-5 <enochpradeesh@gmail.com>
2026-02-11 14:19:49 +0530

    Java Basics -> 02/11/2026

---
+++ StringOpp.java
@@ -0,0 +1,8 @@
+public class StringOpp {
+    public static void main(String[] args) {
+        String str="Java";
+        String str2=str.concat(" is Awesome");
+        System.out.println(str);
+        System.out.println(str2);
+    }
+}
