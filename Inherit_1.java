5e204da1c5c60c52a2bce2b2565c5514be80ff98
PRADEESH-5 <enochpradeesh@gmail.com>
2026-02-12 12:24:18 +0530

    Daily update 02/12/2026 12:24 ~

---
+++ Inherit_1.java
@@ -0,0 +1,18 @@
+class Vehicle {
+    protected String brand = "Ford";        
+    public void honk() {                    
+        System.out.println("Tuut, tuut!");
+    }
+}
+
+class Inherit_1 extends Vehicle {
+    private String modelName = "Mustang";    
+    public static void main(String[] args) {
+
+        Inherit_1 myCar = new Inherit_1();
+
+        myCar.honk();
+
+        System.out.println(myCar.brand + " " + myCar.modelName);
+  }
+}
