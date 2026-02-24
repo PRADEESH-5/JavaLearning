fb895961bf9b2baba23672227636e8cb5de505ef
PRADEESH-5 <enochpradeesh@gmail.com>
2026-02-24 10:04:44 +0530

    Daily update 02/24/2026 10:04 ~

--- SortList.java
+++ SortList.java
@@ -2,6 +2,15 @@ import java.util.*;
 public class SortList {
     public static void main(String[] args) {
         LinkedList<Integer> l=new LinkedList<>();
-
+        l.add(3);
+        l.add(8);
+        l.add(9);
+        l.add(2);
+        l.add(1);
+        System.out.println(l);
+        Collections.sort(l);
+        System.out.println(l);
+        Collections.sort(l,Collections.reverseOrder());
+        System.out.println(l);
     }
 }
