581abc3bafb52f627bca3e2207c05865136f70c5
PRADEESH-5 <enochpradeesh@gmail.com>
2026-02-24 12:00:59 +0530

    Daily update 02/24/2026 12:02 ~

---
+++ TreeSet_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TreeSet_1 {
+    public static void main(String[] args) {
+        TreeSet<Integer> t=new TreeSet<>();
+        t.add(3);
+        t.add(4);
+        t.add(1);
+        t.add(2);
+        t.add(-1);
+
+        System.out.println(t);
+    }
+}
