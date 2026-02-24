ee4fa09dc4bf1911e47caad376c4f46c7cf33fee
PRADEESH-5 <enochpradeesh@gmail.com>
2026-02-24 16:29:25 +0530

    Daily update 02/24/2026 16:29\ ~

--- Generics_1.java
+++ Generics_1.java
@@ -12,6 +12,11 @@ class Box<M>{
 public class Generics_1{
     public static void main(String[] args) {
         Box<Integer> intBox=new Box();
+        intBox.set(10);
+        System.out.println(intBox.get());
 
+        Box<String> stringBox=new Box();
+        stringBox.set("Pradeesh");
+        System.out.println(stringBox.get());
     }
 }
